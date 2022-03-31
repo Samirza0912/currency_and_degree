22c1f73001628a49716c2425d0e2b9497bef853b
Samirza0912 <samirza@code.edu.az>
2022-03-31 14:08:10 +0400

    feat: i add task about currency and degree

---
+++ currency_and_degree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace currency_and_degree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
